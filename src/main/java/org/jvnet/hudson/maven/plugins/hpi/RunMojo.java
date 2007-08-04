5d49a646dda88eebb8d5009cd87359e003216cdb
kohsuke <kohsuke@@dev.java.net>
2007-08-04 16:42:57 +0000

    added about -Djetty.port

--- src/main/java/org/jvnet/hudson/maven/plugins/hpi/RunMojo.java
+++ src/main/java/org/jvnet/hudson/maven/plugins/hpi/RunMojo.java
@@ -1,5 +1,5 @@
 //========================================================================
-//$Id: RunMojo.java 4050 2007-08-04 16:41:43Z kohsuke $
+//$Id: RunMojo.java 4051 2007-08-04 16:42:57Z kohsuke $
 //Copyright 2000-2004 Mort Bay Consulting Pty. Ltd.
 //------------------------------------------------------------------------
 //Licensed under the Apache License, Version 2.0 (the "License");
@@ -41,6 +41,11 @@ import java.util.Set;
  * <p>
  * This only needs the source files to be compiled, so run in the compile phase.
  * </p>
+ *
+ * <p>
+ * To specify the HTTP port, use <tt>-Djetty.port=<i>PORT</i></tt>
+ * </p>
+ * 
  * @goal run
  * @requiresDependencyResolution test
  * @execute phase=compile
