27ef644a216a7bcf3d955d575ad520f9531a2ff2
Jason Dillon <jason@planet57.com>
2011-02-21 16:18:54 -0800

    Repackage com.sonatype.matrix.smoothie -> org.hudsonci.inject

---
+++ hudson-inject/src/main/java/org/hudsonci/inject/injecto/internal/InjectomaticImpl.java
@@ -0,0 +1,138 @@
+/**
+ * The MIT License
+ *
+ * Copyright (c) 2010-2011 Sonatype, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.hudsonci.inject.injecto.internal;
+
+import com.google.inject.Injector;
+import org.hudsonci.inject.SmoothieContainer;
+import org.hudsonci.inject.injecto.Injectable;
+import org.hudsonci.inject.injecto.Injectomatic;
+import org.hudsonci.inject.internal.OID;
+import org.hudsonci.inject.internal.SmoothieContainerImpl;
+import org.hudsonci.inject.internal.plugin.PluginClassLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Default {@link org.hudsonci.inject.injecto.Injectomatic} implementation.
+ *
+ * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ * @since 1.396
+ */
+@Named
+@Singleton
+public class InjectomaticImpl
+    implements Injectomatic
+{
+    private static final Logger log = LoggerFactory.getLogger(InjectomaticImpl.class);
+
+    private final SmoothieContainer container;
+
+    //
+    // FIXME: Probably need to make these guys thread-safe, but not synchronized to avoid perf cost
+    //
+
+    private final Set<Class> registered = new HashSet<Class>();
+
+    private final Set<Class> injectable = new HashSet<Class>();
+
+    private final Set<Class> nonInjectable = new HashSet<Class>();
+
+    @Inject
+    public InjectomaticImpl(final SmoothieContainer container) {
+        assert container != null;
+        this.container = container;
+        register(Injectable.class);
+    }
+
+    public void register(final Class type) {
+        assert type != null;
+        log.debug("Registering type: {}", type);
+        registered.add(type);
+    }
+
+    public boolean isInjectable(final Class type) {
+        assert type != null;
+
+        // See if we have already cached if this type is injectable or not
+        if (injectable.contains(type)) {
+            return true;
+        }
+        else if (nonInjectable.contains(type)) {
+            return false;
+        }
+
+        // See if the type is assignable from a registered type
+        for (Class rtype : registered) {
+            if (rtype.isAssignableFrom(type)) {
+                // Add to injectable type cache
+                injectable.add(type);
+                log.trace("Detected injectable type: {}", type);
+                return true;
+            }
+        }
+
+        // Not injectable; add to cache
+        nonInjectable.add(type);
+
+        return false;
+    }
+
+    public void inject(final Object component) {
+        assert component != null;
+
+        Class type = component.getClass();
+
+        if (!isInjectable(type)) {
+            log.trace("Type not injectable; skipping: {}", type);
+            return;
+        }
+
+        // Find the injector for the component
+        ClassLoader tmp = type.getClassLoader();
+        Injector injector;
+
+        // If the component belongs to a plugin, then use the plugin's injector
+        if (tmp instanceof PluginClassLoader) {
+            PluginClassLoader cl = (PluginClassLoader)tmp;
+            injector = container.injector(cl.getPlugin());
+        }
+        else {
+            // Use the root injector if we did not load from a plugin
+            injector = ((SmoothieContainerImpl)container).rootInjector();
+        }
+
+        if (log.isTraceEnabled()) {
+            log.trace("Injecting: {} ({})", component, OID.get(component));
+        }
+
+        injector.injectMembers(component);
+    }
+}
---
+++ hudson-inject/src/main/java/org/hudsonci/inject/internal/extension/SezPozExtensionModule.java
@@ -0,0 +1,155 @@
+/**
+ * Copyright (c) 2010-2011 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.hudsonci.inject.internal.extension;
+
+import com.google.inject.Binder;
+import com.google.inject.Key;
+import com.google.inject.Module;
+import com.google.inject.Provider;
+import com.google.inject.ProvisionException;
+import com.google.inject.Scopes;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+import hudson.Extension;
+import hudson.ExtensionPoint;
+import hudson.model.Descriptor;
+import net.java.sezpoz.SpaceIndex;
+import net.java.sezpoz.SpaceIndexItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonatype.guice.bean.reflect.ClassSpace;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * Guice {@link Module} that binds types based on SezPoz index.
+ *
+ * @since 1.396
+ */
+@SuppressWarnings( { "unchecked", "rawtypes" } )
+public final class SezPozExtensionModule
+    implements Module
+{
+    private static final Logger log = LoggerFactory.getLogger(SezPozExtensionModule.class);
+
+    // ----------------------------------------------------------------------
+    // Implementation fields
+    // ----------------------------------------------------------------------
+
+    private final ClassSpace space;
+
+    // ----------------------------------------------------------------------
+    // Constructors
+    // ----------------------------------------------------------------------
+
+    public SezPozExtensionModule( final ClassSpace space )
+    {
+        this.space = space;
+    }
+
+    // ----------------------------------------------------------------------
+    // Public methods
+    // ----------------------------------------------------------------------
+
+    public void configure( final Binder binder )
+    {
+        for ( final SpaceIndexItem item : SpaceIndex.load( Extension.class, Object.class, space ) )
+        {
+            try
+            {
+                switch ( item.kind() )
+                {
+                    case TYPE:
+                    {
+                        final Class impl = (Class) item.element();
+                        binder.bind( impl ).in( Scopes.SINGLETON );
+                        bindHierarchy( binder, Key.get( impl ) );
+
+                        break;
+                    }
+                    case METHOD:
+                    {
+                        final Method method = (Method) item.element();
+                        final Named name = Names.named( method.getDeclaringClass().getName() + '.' + method.getName() );
+                        final Key key = Key.get( method.getReturnType(), name );
+                        bindProvider(binder, item, key);
+
+                        break;
+                    }
+                    case FIELD:
+                    {
+                        final Field field = (Field) item.element();
+                        final Named name = Names.named( field.getDeclaringClass().getName() + '.' + field.getName() );
+                        final Key key = Key.get( field.getType(), name );
+                        bindProvider(binder, item, key);
+
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+            catch ( final Throwable e )
+            {
+                log.error("Failed to bind item: {}", item, e);
+            }
+        }
+    }
+
+    private void bindProvider(final Binder binder, final SpaceIndexItem item, final Key key) {
+        binder.bind(key).toProvider(new Provider() {
+            public Object get() {
+                try {
+                    return item.instance();
+                } catch (final InstantiationException e) {
+                    throw new ProvisionException(e.toString(), e);
+                }
+            }
+        }).in(Scopes.SINGLETON);
+        bindHierarchy(binder, key);
+    }
+
+    private static void bindHierarchy( final Binder binder, final Key rootKey )
+    {
+        final Class root = rootKey.getTypeLiteral().getRawType();
+
+        Annotation qualifier = rootKey.getAnnotation();
+        if ( null == qualifier )
+        {
+            qualifier = Names.named( root.getName() );
+        }
+
+        for ( Class clazz = root; clazz != Object.class; clazz = clazz.getSuperclass() )
+        {
+            if ( clazz != root && ExtensionPoint.class.isAssignableFrom( clazz ) )
+            {
+                binder.bind( clazz ).annotatedWith( qualifier ).to( rootKey );
+            }
+
+            if ( Descriptor.class.isAssignableFrom( clazz ) )
+            {
+                binder.bind( Descriptor.class ).annotatedWith( qualifier ).to( rootKey );
+            }
+
+            for ( final Class<?> iface : clazz.getInterfaces() )
+            {
+                if ( ExtensionPoint.class.isAssignableFrom( iface ) )
+                {
+                    binder.bind( iface ).annotatedWith( qualifier ).to( rootKey );
+                }
+            }
+        }
+    }
+}
