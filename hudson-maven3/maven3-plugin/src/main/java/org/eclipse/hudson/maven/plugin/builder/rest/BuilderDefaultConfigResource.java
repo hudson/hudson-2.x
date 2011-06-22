b68e05b5bed4afa25003fb4eacd67ca5fd1926cc
Stuart McCulloch <mcculls@gmail.com>
2011-06-22 09:54:26 +0100

    Make sure REST resource classes are registered with the DI container

--- hudson-maven3/maven3-plugin/src/main/java/org/eclipse/hudson/maven/plugin/builder/rest/BuilderDefaultConfigResource.java
+++ hudson-maven3/maven3-plugin/src/main/java/org/eclipse/hudson/maven/plugin/builder/rest/BuilderDefaultConfigResource.java
@@ -16,18 +16,17 @@
 
 package org.eclipse.hudson.maven.plugin.builder.rest;
 
-import org.eclipse.hudson.maven.plugin.Constants;
-import org.eclipse.hudson.maven.plugin.builder.MavenBuilderDescriptor;
-import org.eclipse.hudson.maven.plugin.builder.MavenBuilderService;
-import org.eclipse.hudson.maven.model.config.BuildConfigurationDTO;
-
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
+import org.eclipse.hudson.maven.model.config.BuildConfigurationDTO;
+import org.eclipse.hudson.maven.plugin.Constants;
+import org.eclipse.hudson.maven.plugin.builder.MavenBuilderDescriptor;
+import org.eclipse.hudson.maven.plugin.builder.MavenBuilderService;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -39,6 +38,7 @@ import static javax.ws.rs.core.MediaType.APPLICATION_XML;
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  * @since 2.1.0
  */
+@Named
 @Path(Constants.URI_PREFIX + "/builderDefaultConfig")
 @Produces({APPLICATION_JSON, APPLICATION_XML})
 public class BuilderDefaultConfigResource
