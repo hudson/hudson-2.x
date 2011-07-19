a76fdc4b9870cae57f160668cfa487b90d6299bb
Winston Prakash <winston.prakash@gmail.com>
2011-07-19 10:35:56 -0700

    Adding EPL license header to File with Copyright "2008-2010 Yahoo! Inc."

--- hudson-test-harness/src/test/java/hudson/security/csrf/DefaultCrumbIssuerTest.java
+++ hudson-test-harness/src/test/java/hudson/security/csrf/DefaultCrumbIssuerTest.java
@@ -1,8 +1,18 @@
-/**
+/*******************************************************************************
+ *
  * Copyright (c) 2008-2010 Yahoo! Inc.
- * All rights reserved.
- * The copyrights to the contents of this file are licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: 
+ *
+ *   
+ *       
+ *
+ *******************************************************************************/ 
 
 package hudson.security.csrf;
 
