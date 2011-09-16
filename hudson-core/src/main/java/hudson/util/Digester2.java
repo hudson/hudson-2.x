5dac756d13cb641e677efe66a6a1435d55e59c64
8nevil8 <nevil.nick@gmail.com>
2011-09-16 11:05:28 +0300

    Add legacy commons-digester and commons-lang dependencies for backward compatibility

--- hudson-core/src/main/java/hudson/util/Digester2.java
+++ hudson-core/src/main/java/hudson/util/Digester2.java
@@ -23,15 +23,45 @@
  */
 package hudson.util;
 
-import org.apache.commons.digester3.Digester;
+import hudson.RestrictedSince;
+import org.apache.commons.digester.Digester;
+import org.apache.commons.digester.Rule;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+import org.xml.sax.Attributes;
 
 /**
  * {@link Digester} wrapper to fix the issue DIGESTER-118.
  *
  * @author Kohsuke Kawaguchi
  * @since 1.125
- * @deprecated use {@link Digester} instead
+ * @deprecated use {@link org.apache.commons.digester3.Digester} instead
  * @since 2.1.2
  */
+@Restricted(NoExternalUse.class)
+@RestrictedSince("2.1.2")
 public class Digester2 extends Digester {
+    @Override
+    public void addObjectCreate(String pattern, Class clazz) {
+        addRule(pattern,new ObjectCreateRule2(clazz));
+    }
+
+    private static final class ObjectCreateRule2 extends Rule {
+        private final Class clazz;
+        
+        public ObjectCreateRule2(Class clazz) {
+            this.clazz = clazz;
+        }
+
+        @Override
+        public void begin(String namespace, String name, Attributes attributes) throws Exception {
+            Object instance = clazz.newInstance();
+            digester.push(instance);
+        }
+
+        @Override
+        public void end(String namespace, String name) throws Exception {
+            digester.pop();
+        }
+    }
 }
